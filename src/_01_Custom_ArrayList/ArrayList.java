20efae7400f0c6d8f5326e55125284e7ff9ddc30
hop324 <39838785+hop324@users.noreply.github.com>
2020-09-22 19:54:34 -0700

    Custom Array List (IP)
    I hate generics :+1: :sunglasses: :100:

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,33 +3,37 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	public T t;
+	ArrayList<T> list = new ArrayList<T>();
 	public ArrayList() {
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return list.get(loc);
 	}
 	
 	public void add(T val) {
-		
+		list.add(val);
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list.insert(loc, val);
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list.get(loc).equals(val);
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		list.remove(loc);
 	}
 	
 	public boolean contains(T val) {
-		
-		return false;
+		if(list.contains(val)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
