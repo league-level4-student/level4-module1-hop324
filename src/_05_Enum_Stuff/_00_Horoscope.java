aa21682af02ba8ea2fb3481933f141c8d5dd179b
hop324 <39838785+hop324@users.noreply.github.com>
2020-10-13 19:57:11 -0700

    Horoscope
    :100: :+1: :sunglasses:

--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -1,10 +1,12 @@
 package _05_Enum_Stuff;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 
 public class _00_Horoscope implements ActionListener{
@@ -21,32 +23,48 @@ public class _00_Horoscope implements ActionListener{
 	JButton button11 = new JButton();
 	JButton button12 = new JButton();
 	
-	Zodiac holder;
+	public Zodiac holder;
 	// 1. Create an enum in a separate file called Zodiac that contains a category for
 	//    all 12 zodiac signs.
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	public void horoscope(Zodiac a){
+	public void createUI() {
+		JPanel panel = new JPanel();
 		JFrame frame = new JFrame();
-		JOptionPane panel = new JOptionPane();
 		button1.addActionListener(this);
+		button1.setText("AQUARIUS");
 		button2.addActionListener(this);
+		button2.setText("ARIES");
 		button3.addActionListener(this);
+		button3.setText("CANCER");
 		button4.addActionListener(this);
+		button4.setText("CAPRICORN");
 		button5.addActionListener(this);
+		button5.setText("GEMINI");
 		button6.addActionListener(this);
+		button6.setText("LEO");
 		button7.addActionListener(this);
+		button7.setText("LIBRA");
 		button8.addActionListener(this);
+		button8.setText("PISCES");
 		button9.addActionListener(this);
+		button9.setText("SAGITARIUS");
 		button10.addActionListener(this);
+		button10.setText("SCORPIO");
 		button11.addActionListener(this);
+		button11.setText("TAURUS");
 		button12.addActionListener(this);
+		button12.setText("VIRGO");
 		panel.add(button1);panel.add(button2);panel.add(button3);panel.add(button4);panel.add(button5);panel.add(button6);
 		panel.add(button7);panel.add(button8);panel.add(button9);panel.add(button10);panel.add(button11);panel.add(button12);
 		frame.add(panel);
+		panel.setLayout(new GridLayout(4,3));
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	public void horoscope(Zodiac a){
 		switch(a) {
 			case ARIES:
 				System.out.println("You're gonna be cool");
@@ -90,13 +108,49 @@ public class _00_Horoscope implements ActionListener{
 		}
 	}
 	// 3. Make a main method to test your method
+	public static void main(String [] args) {
+		new _00_Horoscope().createUI();
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == button1) {
-			holder = new Zodiac.PISCES;
+			holder = Zodiac.AQUARIUS;
 		}
-		
+		else if(e.getSource() == button2) {
+			holder = Zodiac.ARIES;
+		}
+		else if(e.getSource() == button3) {
+			holder = Zodiac.CANCER;
+		}
+		else if(e.getSource() == button4) {
+			holder = Zodiac.CAPRICORN;
+		}
+		else if(e.getSource() == button5) {
+			holder = Zodiac.GEMINI;
+		}
+		else if(e.getSource() == button6) {
+			holder = Zodiac.LEO;
+		}
+		else if(e.getSource() == button7) {
+			holder = Zodiac.LIBRA;
+		}
+		else if(e.getSource() == button8) {
+			holder = Zodiac.PISCES;
+		}
+		else if(e.getSource() == button9) {
+			holder = Zodiac.SAGITTARIUS;
+		}
+		else if(e.getSource() == button10) {
+			holder = Zodiac.SCORPIO;
+		}
+		else if(e.getSource() == button11) {
+			holder = Zodiac.TAURUS;
+		}
+		else if(e.getSource() == button12) {
+			holder = Zodiac.VIRGO;
+		}
+		horoscope(holder);
 	}
 	
 }
